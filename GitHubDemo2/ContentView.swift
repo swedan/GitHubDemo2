0cc29e35e7aa04c07e5d34c3833e75d81ab58d5e
Mousa Sweidan <msweidan@hct.ac.ae>
2023-08-09 10:08:13 +0300

    Add another Text with vStack

--- GitHubDemo2/ContentView.swift
+++ GitHubDemo2/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("Gihub Project")
+                .bold()
+                .font(.title)
+            Text("Hello, world!")
+                .padding()
+                .font(.subheadline)
+        }
     }
 }
 
